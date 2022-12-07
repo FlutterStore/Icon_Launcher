1cbbc3773f474bddaccda1f7468cfdaaa80d3e82
Flutter Store <118250909+FlutterStore@users.noreply.github.com>
2022-12-07 17:03:17 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/icon_launcher/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.icon_launcher
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key,});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.green,
+        title: const Text("Home Page",style: TextStyle(fontSize: 15),),
+        centerTitle: true,
+      ),
+      body: const Center(child: Text("WELCOME",style: TextStyle(fontSize: 25,fontWeight: FontWeight.bold),)),
+    );
+  }
+}
